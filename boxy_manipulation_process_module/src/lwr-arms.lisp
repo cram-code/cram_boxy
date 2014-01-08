e4026e05454bd2157a31f59eea9ac976a0a391e1
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-01-08 09:45:02 +0100

    Added rudimentary modelling of left LWR arm in boxy-manip-pm.

--- boxy_manipulation_process_module/src/lwr-arms.lisp
+++ boxy_manipulation_process_module/src/lwr-arms.lisp
@@ -33,17 +33,40 @@
 (defvar *left-arm-config* nil
   "Variable holding the current configuration of the left arm.")
 
+(defparameter *left-beasty-action-name* "/LEFT_BEASTY"
+  "ROS name of the Beasty action server for the left arm.")
+(defparameter *left-simulation-flag* t
+  "Flag indicating whether the left LWR is a simulated arm.")
+(defparameter *left-gravity-vector* #(0 0 1 0 0 0)
+  "_NEGATIV_ 6D acceleration vector indicating in which direction gravity is acting on the
+  left arm. NOTE: Is expressed w.r.t. to the base-frame of the left arm. First translational (x,y,z), then rotational (x,y,z) acceleration.")
+
 (defun init-boxy-manipulation-process-module ()
   "Inits connection to hardware drivers used by the process module."
   (unless *left-arm* 
-    (setf *left-arm* (make-beasty-interface "/LEFT_BEASTY")))
+    (setf *left-arm* (make-beasty-interface *left-beasty-action-name*)))
   (unless *left-arm-config*
-    (setf *left-arm-config* (make-beasty-robot))))
+    (setf *left-arm-config* (init-beasty-robot *left-simulation-flag* 
+                                               *left-gravity-vector*))))
+(defun clean-up-boxy-manipulation-process-module ()
+  (when *left-arm*
+    ;; TODO(Georg): stop arm
+    ;; TODO(Georg): logout with Beasty
+    (setf *left-arm* nil))
+  (when *left-arm-config*
+    (setf *left-arm-config* nil)))
 
 (roslisp-utilities:register-ros-init-function init-boxy-manipulation-process-module)
+(roslisp-utilities:register-ros-cleanup-function clean-up-boxy-manipulation-process-module)
 ;; TODO(Georg): add clean-up-function with LOGOUT
 
-(defun make-beasty-robot ()
-  "Creates and returns an instance of class `beasty-robot'."
-  
-)
+(defun init-beasty-robot (simulated-p base-acceleration)
+  "Creates, inits, and returns an instance of class `beasty-robot'. `simulated-p' signals
+ whether this a simulated LWR, and the 6D-vector `base-acceleration' denotes the _negative_
+ external acceleration acting on the robot."
+  (declare (type boolean simulated-p)
+           (type (vector number 6) base-acceleration))
+  (let ((robot (make-instance 'beasty-robot)))
+    (setf (simulation-flag robot) simulated-p)
+    (setf (base-acceleration (base-configuration robot))  base-acceleration)
+    robot))
